b50fd37e02b8af6277aed95f6b0215ae159603c0
KHAV <khweld454@gmail.com>
2018-07-13 00:06:23 +0300

    Update Loader.php

--- src/TimeZone/khavmc/Loader.php
+++ src/TimeZone/khavmc/Loader.php
@@ -2,8 +2,38 @@
 
 namespace TimeZone\khavmc;
 
+use pocketmine\plugin\PluginBase;
+use pocketmine\utils\TextFormat;
+
 class Loader extends PluginBase{
 
 
-
+  public function onEnable(){
+    $this->getServer()->getLogger()->info(TextFormat::GREEN."TimeZone Plugin Enabled!");
+            $this->getServer()->getCommandMap()->register("timezone", new TimeZoneCommand($this));
+    $this->saveDefaultConfig();
+    $this->reloadConfig();
+  }
+  
+  public function getRegionTime(string $region, Player $player){
+  date_default_timezone_set($region);
+  $date = date("Y/n/j");
+  $time = date("G:i:s");
+    $search = array(
+                    '{date}',
+                    '{time}'
+                );
+                $replace = array(
+                    $date,
+                    $time
+                );
+                    $time_msg = str_replace($search, $replace, $this->getConfig()->getAll(){"time_message"});
+                if($this->getConfig()->get("message_type") == "msg") {
+                $player->sendMessage(FMT::colorMessage($time_msg));
+  }elseif($this->getConfig()->get("message_type") == "tip") {
+                
+                }elseif($this->getConfig()->get("message_type") == "popup") {
+           //ToDo     
+                }
+  }
 }
